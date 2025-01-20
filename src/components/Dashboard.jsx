35473b02297b4735706ac37c2f5646a1d5789b2f
Mehran Shahani <mehranshahani6@gmail.com>
2025-01-21 00:26:25 +0330

    Adding task description feature and filtering by completed and pending feature

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react"
 import TaskList from "./TaskList"
 import { nanoid } from "nanoid"
-import { clsx } from "clsx"
 
 /* 
  -saving tasks from users into state (done)
@@ -10,9 +9,10 @@ import { clsx } from "clsx"
  -adding functionality to checkboxes to change checked tasks in state (done)
  -adding a delete button next to each task (done)
  -adding functionality to delete buttons to delete tasks from tasksList (done)
- -filter tasks by completed and pending status
- -adding a section for buttons to add filtering tasks
- -
+ -filter tasks by completed and pending status (done)
+ -adding a section for buttons to add filtering tasks (done)
+ -adding a description input for each task
+ -adding a calender for each task
 */
 
 export default function Dashboard() {
@@ -24,19 +24,19 @@ export default function Dashboard() {
     const filteredTasks = 
             filterStatus === "all" ? tasksList : tasksList.filter(task => filterStatus === "completed" ? task.checked : !task.checked)
 
-    //Dynamic classNames
-        const filterButtonsClasses = clsx({
-            "active-filter": filterStatus === "all" || filterStatus === "completed" || filterStatus === "pending"
-        })
-    
-    //Grabbing tasks from task-form and putting it in the tasksList state array
+    //Grabbing tasks from task-form and putting it in the tasksList state 
     function addTasks(formData) {
         const newTask = formData.get("task")
-        setTasksList(prevTasksList => [...prevTasksList, {
-            id: nanoid(),
-            task: newTask,
-            checked: false
-        }])
+        if (newTask === "") {
+            return
+        } else {
+            setTasksList(prevTasksList => [...prevTasksList, {
+                id: nanoid(),
+                task: newTask.trim(),
+                checked: false,
+                description: ""
+            }])
+        }
     }
 
     //Controlling checkboxes in our state
@@ -51,6 +51,15 @@ export default function Dashboard() {
         setTasksList(prevTasksList => prevTasksList.filter(taskObj => taskObj.id !== id))
     }
 
+    //Grabbing tasks description from description-from and adding in the tasksList state
+    function handleDescription(id, formData) {
+        const newDesc = formData.get("description")
+                setTasksList(prevTasksList => prevTasksList.map(taskObj => (
+                id === taskObj.id ? {...taskObj, description: newDesc} : taskObj
+            ))) 
+    }
+
+    console.log(tasksList)
     return (
         <main className="main">
             <form action={addTasks} 
@@ -66,34 +75,39 @@ export default function Dashboard() {
                 </label>
                 <button className="add-task-btn">ADD</button>
             </form>
-            {tasksList.length > 0 && <TaskList 
-                tasksList={filteredTasks}
-                handleCheckboxChange={handleCheckboxChange}
-                handleDelete={handleDelete}
-            />}
-            <section className="filter-container">
-                <span>
-                    Filter By:
-                    <button
-                        onClick={() => setFilterStatus("completed")}
-                        className={filterButtonsClasses}
-                    >
-                        Completed Tasks
-                    </button>
-                    <button
-                        onClick={() => setFilterStatus("pending")}
-                        className={filterButtonsClasses}
-                    >
-                        Pending Tasks
-                    </button>
-                    <button
-                        onClick={() => setFilterStatus("all")}
-                        className={filterButtonsClasses}
-                    >
-                        All
-                    </button>
-                </span>
-            </section>
+            {tasksList.length > 0 && (
+                <section className="tasks-section">
+                    <section className="filter-container">
+                        <span>
+                            Filter By:
+                            <button
+                                onClick={() => setFilterStatus("completed")}
+                                className={filterStatus === "completed" ? "active-filter" : ""}
+                            >
+                                Completed Tasks
+                            </button>
+                            <button
+                                onClick={() => setFilterStatus("pending")}
+                                className={filterStatus === "pending" ? "active-filter" : ""}
+                            >
+                                Pending Tasks
+                            </button>
+                            <button
+                                onClick={() => setFilterStatus("all")}
+                                className={filterStatus === "all" ? "active-filter" : ""}
+                            >
+                                All
+                            </button>
+                        </span>
+                    </section>
+                    <TaskList 
+                    tasksList={filteredTasks}
+                    handleCheckboxChange={handleCheckboxChange}
+                    handleDelete={handleDelete}
+                    handleDescription={handleDescription}
+                    />
+                </section>
+            )}
         </main>
     )
 }
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,6 +1,14 @@
+import { useState } from "react"
 import deleteIcon from "../assets/delete-icon.svg"
 
-export default function TaskList({tasksList, handleCheckboxChange, handleDelete}) {
+export default function TaskList({tasksList, handleCheckboxChange, handleDelete, handleDescription}) {
+    //Passing description value to the parent function
+    function handleSubmitDescription(e, id) {
+        e.preventDefault()
+        const formData = new FormData(e.target)
+        handleDescription(id, formData)
+    }
+
     //Mapping over added tasks and displaying them
     const taskElements = tasksList.map(({id, task}) => (
         <li key={id}
@@ -8,18 +16,24 @@ export default function TaskList({tasksList, handleCheckboxChange, handleDelete}
         >
             {task}
             <div className="task-item-btn-container">
-                <label>
-                    Completed:
-                    <input 
+                <label>Completed:</label>
+                <input 
                         type="checkbox"
                         onChange={() => handleCheckboxChange(id)}
-                    />
-                </label>
+                        className="task-checkbox"
+                />
                 <button
                     onClick={() => handleDelete(id)}
                 >
                     <img src={deleteIcon} alt="delete icon" />
                 </button>
+                <div className="task-description-container">
+                    <form onSubmit={(e) => handleSubmitDescription(e, id)}>
+                        <label htmlFor={`description-${id}`}>Description:</label>
+                        <textarea name="description" id={`description-${id}`}></textarea>
+                        <button type="submit">Add Description</button>
+                    </form>
+                </div>
             </div>
         </li>
     ))
